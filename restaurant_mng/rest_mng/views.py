a94ba0d25e0b864b35c19d222dc6f1cee497805f
ITemci <temciucvanea@gmail.com>
2024-11-28 16:38:23 +0000

    first commit

---
+++ restaurant_mng/rest_mng/views.py
@@ -0,0 +1,14 @@
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+# from .models import User, Email
+
+# Create your views here.
+def index(request):
+    HttpResponse("hello")
