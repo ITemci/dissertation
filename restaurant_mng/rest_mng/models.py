a87b9d63b0799a14a06095b0ac1b9c69ad44f75b
ITemci <temciucvanea@gmail.com>
2024-12-03 21:23:54 +0000

    created the users model

--- restaurant_mng/rest_mng/models.py
+++ restaurant_mng/rest_mng/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Product(models.Model):
+    prod_name = models.CharField(max_length=15)
+    prod_desc = models.CharField(max_length=255)
+    price = models.IntegerField()
+    category = models.CharField(max_length=15, default="N/A")
+    date = models.DateTimeField(auto_now_add=True)
+    in_stock = models.BooleanField(default=True)
+    image = models.CharField(max_length=255, blank=True)
+    favorite = models.ManyToManyField(User, blank= True, null= True, related_name="favorite")
--- restaurant_mng/restaurant_mng/urls.py
+++ restaurant_mng/restaurant_mng/urls.py
@@ -19,5 +19,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rest_mng.urls'))
+    path('', include('rest_mng.urls')),
+
 ]
